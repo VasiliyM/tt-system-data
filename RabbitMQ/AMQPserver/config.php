7c2677446ace7beb7858d8c3c3ca7144331a59a2
panches <panches@ukr.net>
2015-09-02 15:23:20 +0300

    файлы AMQP сервера

---
+++ RabbitMQ/AMQPserver/config.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+define('HOST', 'spotted-monkey.rmq.cloudamqp.com');
+define('PORT', 5672);
+define('USER', 'qfdkycde');
+define('PASS', 'wblq8VolJxEzMRCxCOttm_dYYFqdQqGh');
+define('VHOST', 'qfdkycde');
+
+//If this is enabled you can see AMQP output on the CLI
+define('AMQP_DEBUG', true);
+
+// define for DB
+	$host = "localhost";
+    $user = "marusyak";
+    $password = "Alex1974";
+    $db = "klients";
