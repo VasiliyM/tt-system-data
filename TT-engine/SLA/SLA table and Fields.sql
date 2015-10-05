ae2e8ec8bf7f57015df3487fa111a4a3cce377c6
VasMor <rf400rt@gmail.com>
2015-10-06 00:43:50 +0300

    Полное добавление двух полей в СК и каталога SLA

--- TT-engine/SLA/SLA table and Fields.sql
+++ TT-engine/SLA/SLA table and Fields.sql
@@ -10,12 +10,14 @@ CREATE TABLE IF NOT EXISTS `tab_sla_net_data` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `lbl` varchar(3) NOT NULL COMMENT 'Краткое название для системы ТТ и оператора',
+  `dflt` int(1) NOT NULL DEFAULT '0' COMMENT 'по умолчанию',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=6 ;
 
-INSERT INTO `tab_sla_net_data` (`id`, `name`, `lbl`) VALUES
-(1, 'Преміум', 'Pre'),
-(2, 'Бізнес', 'Bsn'),
-(3, 'Стандарт', 'Std'),
-(4, 'Супутниковий', 'Stl'),
-(5, 'Легкий', 'Lgh');
+
+INSERT INTO `tab_sla_net_data` (`id`, `name`, `lbl`, `dflt`) VALUES
+(1, 'Преміум', 'Pre', 0),
+(2, 'Бізнес', 'Bsn', 0),
+(3, 'Стандарт', 'Std', 1),
+(4, 'Супутниковий', 'Stl', 0),
+(5, 'Легкий', 'Lgh', 0);
