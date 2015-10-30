4f7b3efbd6a2e653c60ecf17b7797da217f2169f
VasMor <rf400rt@gmail.com>
2015-10-30 14:50:06 +0200

    Добавил признак выделения жарным SLA  в интерфейсе при открытии ТТ

--- TT-engine/SLA/SLA table and Fields.sql
+++ TT-engine/SLA/SLA table and Fields.sql
@@ -5,19 +5,19 @@ ADD `sla_id` INT NOT NULL DEFAULT '0' COMMENT 'id on SLAs table' AFTER `sla_d`;
 
 # Добавляем новую таблицу с экземплярами SLA для СК, поля буду добавлять, пока такие:
 
-DROP TABLE IF EXISTS `tab_sla_net_data`;
 CREATE TABLE IF NOT EXISTS `tab_sla_net_data` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `lbl` varchar(3) NOT NULL COMMENT 'Краткое название для системы ТТ и оператора',
   `dflt` int(1) NOT NULL DEFAULT '0' COMMENT 'по умолчанию',
+  `bld` int(11) NOT NULL DEFAULT '0' COMMENT 'if 1 - text in BDSE bold',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=7 ;
 
 
-INSERT INTO `tab_sla_net_data` (`id`, `name`, `lbl`, `dflt`) VALUES
-(1, 'Преміум', 'Pre', 0),
-(2, 'Бізнес', 'Bsn', 0),
-(3, 'Стандарт', 'Std', 1),
-(4, 'Супутниковий', 'Stl', 0),
-(5, 'Легкий', 'Lgh', 0);
+INSERT INTO `tab_sla_net_data` (`id`, `name`, `lbl`, `dflt`, `bld`) VALUES
+(1, 'Преміум', 'Pre', 0, 1),
+(2, 'Бізнес', 'Bsn', 0, 1),
+(3, 'Стандарт', 'Std', 1, 0),
+(4, 'Супутник/радіо', 'S/W', 0, 0),
+(5, 'Початковий', 'Sta', 0, 0),
